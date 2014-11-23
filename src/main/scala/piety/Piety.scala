f99b90358aa4e3f5946a96c79deaa3209b2592eb
Tyler Yates <tyler.yates@utexas.edu>
2014-11-23 16:17:44 -0600

    Basic image reading

--- src/main/scala/piety/Piety.scala
+++ src/main/scala/piety/Piety.scala
@@ -1,8 +1,17 @@
 package piety
 
+import javax.imageio.ImageIO
+import java.io.File
+
 object Piety {
 
   def main(args: Array[String]): Unit = {
-    println("Hello World!")
+    processImage("src"+File.separator+"main"+File.separator+"resources"+File.separator+"scala-logo.jpg")
+  }
+  
+  def processImage(path: String): Unit = {
+    val image = ImageIO.read(new File(path))
+    
+    println(image)
   }
 }
