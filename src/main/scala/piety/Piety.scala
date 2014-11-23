8112b09af71cac5efce1e8a1578d415b7fc4144c
Tyler Yates <tyler.yates@utexas.edu>
2014-11-23 16:39:54 -0600

    Add codel size as a parameter

--- src/main/scala/piety/Piety.scala
+++ src/main/scala/piety/Piety.scala
@@ -6,10 +6,10 @@ import java.io.File
 object Piety {
 
   def main(args: Array[String]): Unit = {
-    processImage("src"+File.separator+"main"+File.separator+"resources"+File.separator+"hello_world.gif")
+    processImage("src"+File.separator+"main"+File.separator+"resources"+File.separator+"hello_world.gif", 11)
   }
   
-  def processImage(path: String): Unit = {
+  def processImage(path: String, codelSize: Int): Unit = {
     val image = ImageIO.read(new File(path))
     
     println(image)
