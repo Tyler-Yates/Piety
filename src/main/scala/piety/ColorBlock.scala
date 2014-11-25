ae96d71df1cb02c7bb178e91fc926df5fc893a81
Tyler Yates <tyler.yates@utexas.edu>
2014-11-24 19:39:31 -0600

    Add eight neighbors to ColorBlock class

--- src/main/scala/piety/ColorBlock.scala
+++ src/main/scala/piety/ColorBlock.scala
@@ -2,4 +2,12 @@ package piety
 
 class ColorBlock {
 
+  var rightLeft: ColorBlock = null
+  var rightRight: ColorBlock = null
+  var downLeft: ColorBlock = null
+  var downRight: ColorBlock = null
+  var leftLeft: ColorBlock = null
+  var leftRight: ColorBlock = null
+  var upLeft: ColorBlock = null
+  var upRight: ColorBlock = null
 }
