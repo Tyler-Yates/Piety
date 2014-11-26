792ce8d62a3746fb5bd3c9561dbb3a749c981556
Tyler Yates <tyler.yates@utexas.edu>
2014-11-25 20:51:33 -0600

    Create PietColor class to represent the hue and lightness of a codel

--- src/main/scala/piety/Codel.scala
+++ src/main/scala/piety/Codel.scala
@@ -1,87 +1,17 @@
 package piety
 
 import java.awt.Color
-import Hue._
-import Lightness._
 
 /**
  * Represents a single codel in a Piet program.
  */
 class Codel(val color: Color, val row: Int, val col: Int) {
-  var hue: Hue = calculateHue(color)
-  var lightness: Lightness = calculateLightness(color)
+  var pietColor = new PietColor(color)
 
   var r: Int = row
   var c: Int = col
 
   var parent: ColorBlock = null
-
-  def calculateHue(color: Color): Hue = {
-    color match {
-      case LIGHT_RED     => return Hue.Red
-      case RED           => return Hue.Red
-      case DARK_RED      => return Hue.Red
-
-      case LIGHT_YELLOW  => return Hue.Yellow
-      case YELLOW        => return Hue.Yellow
-      case DARK_YELLOW   => return Hue.Yellow
-
-      case LIGHT_GREEN   => return Hue.Green
-      case GREEN         => return Hue.Green
-      case DARK_GREEN    => return Hue.Green
-
-      case LIGHT_CYAN    => return Hue.Cyan
-      case CYAN          => return Hue.Cyan
-      case DARK_CYAN     => return Hue.Cyan
-
-      case LIGHT_BLUE    => return Hue.Blue
-      case BLUE          => return Hue.Blue
-      case DARK_BLUE     => return Hue.Blue
-
-      case LIGHT_MAGENTA => return Hue.Magenta
-      case MAGENTA       => return Hue.Magenta
-      case DARK_MAGENTA  => return Hue.Magenta
-
-      case BLACK         => return Hue.Black
-
-      case _             => return Hue.White
-    }
-  }
-
-  def calculateLightness(color: Color): Lightness = {
-    color match {
-      case LIGHT_RED     => return Lightness.Light
-      case LIGHT_YELLOW  => return Lightness.Light
-      case LIGHT_GREEN   => return Lightness.Light
-      case LIGHT_CYAN    => return Lightness.Light
-      case LIGHT_BLUE    => return Lightness.Light
-      case LIGHT_MAGENTA => return Lightness.Light
-
-      case RED           => return Lightness.Normal
-      case YELLOW        => return Lightness.Normal
-      case GREEN         => return Lightness.Normal
-      case CYAN          => return Lightness.Normal
-      case BLUE          => return Lightness.Normal
-      case MAGENTA       => return Lightness.Normal
-
-      case DARK_RED      => return Lightness.Dark
-      case DARK_YELLOW   => return Lightness.Dark
-      case DARK_GREEN    => return Lightness.Dark
-      case DARK_CYAN     => return Lightness.Dark
-      case DARK_BLUE     => return Lightness.Dark
-      case DARK_MAGENTA  => return Lightness.Dark
-
-      case _             => return Lightness.Normal
-    }
-  }
-
-  def getHue(): Hue = {
-    return hue
-  }
-
-  def getLightness(): Lightness = {
-    return lightness
-  }
 
   def getRow(): Int = {
     return r
@@ -90,13 +20,17 @@ class Codel(val color: Color, val row: Int, val col: Int) {
   def getColumn(): Int = {
     return c
   }
+  
+  def getColor(): PietColor = {
+    return pietColor
+  }
 
   override def toString(): String = {
-    return "Hue: " + hue + " Lightness: " + lightness
+    return "Row: " + r + " Column: " + c + " Color: " + pietColor.toString()
   }
 
   def equals(other: Codel): Boolean = {
-    return getHue() == other.getHue() && getLightness() == other.getLightness()
+    return pietColor.equals(other.getColor())
   }
 
   def setParent(par: ColorBlock) = {
---
+++ src/main/scala/piety/PietColor.scala
@@ -0,0 +1,86 @@
+package piety
+
+import java.awt.Color
+import Hue._
+import Lightness._
+
+class PietColor(val color: Color) {
+
+  var hue: Hue = calculateHue(color)
+  var lightness: Lightness = calculateLightness(color)
+
+  def calculateHue(color: Color): Hue = {
+    color match {
+      case LIGHT_RED     => return Hue.Red
+      case RED           => return Hue.Red
+      case DARK_RED      => return Hue.Red
+
+      case LIGHT_YELLOW  => return Hue.Yellow
+      case YELLOW        => return Hue.Yellow
+      case DARK_YELLOW   => return Hue.Yellow
+
+      case LIGHT_GREEN   => return Hue.Green
+      case GREEN         => return Hue.Green
+      case DARK_GREEN    => return Hue.Green
+
+      case LIGHT_CYAN    => return Hue.Cyan
+      case CYAN          => return Hue.Cyan
+      case DARK_CYAN     => return Hue.Cyan
+
+      case LIGHT_BLUE    => return Hue.Blue
+      case BLUE          => return Hue.Blue
+      case DARK_BLUE     => return Hue.Blue
+
+      case LIGHT_MAGENTA => return Hue.Magenta
+      case MAGENTA       => return Hue.Magenta
+      case DARK_MAGENTA  => return Hue.Magenta
+
+      case BLACK         => return Hue.Black
+
+      case _             => return Hue.White
+    }
+  }
+
+  def calculateLightness(color: Color): Lightness = {
+    color match {
+      case LIGHT_RED     => return Lightness.Light
+      case LIGHT_YELLOW  => return Lightness.Light
+      case LIGHT_GREEN   => return Lightness.Light
+      case LIGHT_CYAN    => return Lightness.Light
+      case LIGHT_BLUE    => return Lightness.Light
+      case LIGHT_MAGENTA => return Lightness.Light
+
+      case RED           => return Lightness.Normal
+      case YELLOW        => return Lightness.Normal
+      case GREEN         => return Lightness.Normal
+      case CYAN          => return Lightness.Normal
+      case BLUE          => return Lightness.Normal
+      case MAGENTA       => return Lightness.Normal
+
+      case DARK_RED      => return Lightness.Dark
+      case DARK_YELLOW   => return Lightness.Dark
+      case DARK_GREEN    => return Lightness.Dark
+      case DARK_CYAN     => return Lightness.Dark
+      case DARK_BLUE     => return Lightness.Dark
+      case DARK_MAGENTA  => return Lightness.Dark
+
+      case _             => return Lightness.Normal
+    }
+  }
+
+  def getHue(): Hue = {
+    return hue
+  }
+
+  def getLightness(): Lightness = {
+    return lightness
+  }
+
+  def equals(other: PietColor): Boolean = {
+    return getHue() == other.getHue() && getLightness() == other.getLightness()
+  }
+
+  override def toString(): String = {
+    return getLightness() + " " + getHue()
+  }
+}
