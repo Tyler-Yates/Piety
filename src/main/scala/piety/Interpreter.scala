721be0a70b0e697d4714553e6086e25226f3686a
Peter Ten Eyck <pwteneyck@gmail.com>
2014-11-25 23:04:27 -0600

    added stubs for things and ints for directions.

--- src/main/scala/piety/Interpreter.scala
+++ src/main/scala/piety/Interpreter.scala
@@ -1,5 +1,24 @@
 package piety
 
 object Interpreter {
+	final var up 	= 0
+	final var right = 1
+	final var down 	= 2
+	final var left 	= 3
 
+	var directionPointer = right
+	var codelChooser = left
+	
+	def execute(prog: Program) = {
+		// Piet execution begins in the upper left codel
+		var startCodel = prog.getCodel(0, 0)
+	}
+	
+	def doInstruction(hueChange: Int, lightnessChange: Int) {
+	  
+	}
+	
+	def main(args: Array[String]): Unit = {
+    	  println(directionPointer)
+    }
 }
