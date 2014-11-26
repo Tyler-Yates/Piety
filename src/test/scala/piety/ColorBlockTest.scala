f8f8e81d51a6d686987b1fb8c622a450c21ef056
Tyler Yates <tyler.yates@utexas.edu>
2014-11-26 00:36:47 -0600

    Add unit test for the merge method

--- src/test/scala/piety/ColorBlockTest.scala
+++ src/test/scala/piety/ColorBlockTest.scala
@@ -7,6 +7,7 @@ import org.junit.Before
 
 import javax.imageio.ImageIO
 import java.io.File
+import java.awt.Color
 
 class ColorBlockTest extends AssertionsForJUnit {
 
@@ -39,10 +40,10 @@ class ColorBlockTest extends AssertionsForJUnit {
     val codelLowerRight = prog.codels(2)(2)
     val codelUpperRight = prog.codels(0)(2)
     // Test that the codels are in the same color block
-    assertEquals(codelLowerLeft.getParent(), codelLowerRight.getParent())
-    assertEquals(codelLowerLeft.getParent(), codelUpperRight.getParent())
+    assertSame(codelLowerLeft.getParent(), codelLowerRight.getParent())
+    assertSame(codelLowerLeft.getParent(), codelUpperRight.getParent())
     val block2 = codelLowerLeft.getParent()
-    assertNotEquals(block1, block2)
+    assertNotSame(block1, block2)
     // Test extremes
     assertEquals(codelLowerLeft, block2.leftLeft)
     assertEquals(codelLowerLeft, block2.leftRight)
@@ -52,5 +53,31 @@ class ColorBlockTest extends AssertionsForJUnit {
     assertEquals(codelLowerRight, block2.rightRight)
     assertEquals(codelUpperRight, block2.upLeft)
     assertEquals(codelUpperRight, block2.upRight)
+  }
+  
+  @Test def testMerge() = {
+    val block1: ColorBlock = new ColorBlock(new PietColor(Color.RED))
+    val block2: ColorBlock = new ColorBlock(new PietColor(Color.RED))
+    
+    val codel1 = new Codel(Color.RED, 0, 0)
+    val codel2 = new Codel(Color.RED, 1, 0)
+    val codel3 = new Codel(Color.RED, 0, 1)
+    val codel4 = new Codel(Color.RED, 1, 1)
+    
+    block1.addCodel(codel1)
+    block1.addCodel(codel2)
+    
+    block2.addCodel(codel3)
+    block2.addCodel(codel4)
+    
+    block1.mergeColorBlock(block2)
+    assertSame(block1, codel1.getParent())
+    assertSame(block1, codel2.getParent())
+    assertSame(block1, codel3.getParent())
+    assertSame(block1, codel4.getParent())
+    assertTrue(block1.children.contains(codel1))
+    assertTrue(block1.children.contains(codel2))
+    assertTrue(block1.children.contains(codel3))
+    assertTrue(block1.children.contains(codel4))
   }
 }
