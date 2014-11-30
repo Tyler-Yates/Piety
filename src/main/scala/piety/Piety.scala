cafeefd3e02d18d6e8e55cd086e190fdba110adf
Tyler-Yates <tyler.yates@utexas.edu>
2014-11-29 22:14:14 -0600

    Add command line processing

--- src/main/scala/piety/Piety.scala
+++ src/main/scala/piety/Piety.scala
@@ -7,13 +7,33 @@ import java.awt.Color
 object Piety {
 
   def main(args: Array[String]): Unit = {
-    processImage("src"+File.separator+"main"+File.separator+"resources"+File.separator+"fibbig.gif", 1)
+    if (args.length == 2) {
+      try {
+        val filepath = args(0)
+
+        if (new File(filepath).exists()) {
+          val codelSize = args(1).toInt
+
+          if (codelSize > 0) {
+            processImage(filepath, codelSize)
+          } else {
+            println("Codel size must be greater than zero")
+          }
+        } else {
+          println("File does not exist")
+        }
+      } catch {
+        case e: NumberFormatException => println("Second argument must be a number representing the codel size of the program")
+      }
+    } else {
+      println("Program requires two input arguments: [program file path] [codel size]")
+    }
   }
-  
+
   def processImage(path: String, codelSize: Int): Unit = {
     val image = ImageIO.read(new File(path))
     val prog = new Program(image, codelSize)
-    
+
     Interpreter.execute(prog)
   }
 }
