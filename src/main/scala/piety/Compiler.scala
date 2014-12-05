8d0e6320d32991e15e74d123af50a59c30642bbc
Peter Ten Eyck <pwteneyck@gmail.com>
2014-12-05 16:02:48 -0600

    Added support for giant Piet programs in the compiler

--- src/main/scala/piety/Compiler.scala
+++ src/main/scala/piety/Compiler.scala
@@ -11,6 +11,10 @@ import Hue._
  */
 object Compiler {
   
+  val maxCodeBlockSize = 3000
+  val methodName = "piet"
+  var split = false
+  
   def main(args: Array[String]): Unit = {
     if (args.length == 3) {
       try {
@@ -38,20 +42,50 @@ object Compiler {
    * code to the given PrintStream.
    */
   def compileImage(img: BufferedImage, codelSize: Int, out: String): Unit = {
+    
     val output = new PrintStream(new File(out))
     val arrayColumns = img.getWidth() / codelSize
     val arrayRows = img.getHeight() / codelSize
+    val codeBlockSize = (maxCodeBlockSize / arrayColumns) * arrayColumns
     
+    val codeBlocks = (arrayColumns * arrayRows) / codeBlockSize + 1
+    
+    if(arrayColumns * arrayRows > maxCodeBlockSize)
+    	split = true
+    
+    var currentCodeBlock = 0
+    var currentCodel = 0
+    val name = out.substring(out.lastIndexOf("\\")+1, out.lastIndexOf("."))
+    
+    // top of the scala file is always the same
     output.print("import piety._\r\n\r\n")
-    output.print("object " + out.substring(out.lastIndexOf("\\")+1, out.lastIndexOf(".")) + " extends PietDSL {\r\n")
+    output.print("object " + name + " extends PietDSL {\r\n")
     output.print("\tdef main(args: Array[String]): Unit = {\r\n")
     output.print("\t\tBLANK_SPACE\r\n")
     output.print("\t\tROWS(" + arrayRows + ")\r\n")
     output.print("\t\tCOLUMNS(" + arrayColumns + ")\r\n")
     
+    if (split)
+    	for(i <- 0 until codeBlocks)
+    		output.print("\t\t" + methodName + i + "()\r\n")
+    	
+    if (split) {
+    	output.print("\t\tPIET()\r\n")
+    	output.print("\t}\t\r\n")
+    	output.print(methodHeader(currentCodeBlock))
+    }
+    
     for (r <- 0 until arrayRows) {
       output.print("\t\t")
       for (c <- 0 until arrayColumns) {
+        if (split && currentCodel == codeBlockSize) {
+        	currentCodel = 0
+        	currentCodeBlock += 1
+        	output.print("\r\n\t}\r\n")
+        	output.print(methodHeader(currentCodeBlock))
+        }
+        
+        
         val x = c * codelSize
         val y = r * codelSize
         val codelColor = new Color(img.getRGB(x, y))
@@ -84,11 +118,18 @@ object Compiler {
           case BLACK         => output.print("bb;")
           case _             => output.print("ww;")
         }
+        currentCodel += 1
       }
       output.print("\r\n")
     }
-    output.print("\t\tPIET()\r\n")
-    output.print("\t}\r\n")
+    if (split)
+    	output.print("\t}\r\n")
+    else
+    	output.print("\t\tPIET()\r\n")
     output.print("}\r\n\r\n")
+  }
+  
+  def methodHeader(i: Int): String = {
+    return "\tdef " + methodName + i + "(): Unit = {\r\n\t\t"
   }
 }
